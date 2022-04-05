9559c19e42500dede09d9e6e576bb176a1cc4aca
oguzhan kucukonder <kucukonderoguzhan@gmail.com>
2022-04-05 13:53:25 +0300

    bootum

---
+++ src/calismalarim/KdvHesaplama.java
@@ -0,0 +1,17 @@
+package calismalarim;
+
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        // KDV hesaplama
+
+        double tutar,kdvliFiyat,kdv=0.8;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen tutarı giriniz : ");
+        tutar=scan.nextDouble();
+        kdvliFiyat=tutar + (tutar*kdv);
+        System.out.println("Urunun kdv li fiyatı : " + kdvliFiyat);
+
+}
+    }
